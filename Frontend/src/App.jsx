83f2a3e9212eb92415ac96d61cb172a1503b1ea4
moumen <mohamedaminemoumen7@gmail.com>
2024-04-22 13:00:18 +0100

    v-1

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,7 +1,9 @@
-import './App.css'
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import LandingPage from "./pages/LandingPage/LandingPage";
 
 function App() {
-    
+  return <LandingPage />;
 }
 
-export default App
+export default App;
